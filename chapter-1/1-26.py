cd9f52caefbf18b2d05da3d0dd84765969b23f6c
JP Doherty <jpdoherty90@gmail.com>
2017-10-02 13:51:59 -0500

    added 1-26

---
+++ chapter-1/1-26.py
@@ -0,0 +1,11 @@
+def arithmetic(a, b, c):
+    if a + b == c:
+        print "a + b == c"
+    if a - b == c:
+        print "a - b == c"
+    if a * b == c:
+        print "a * b == c"
+
+
+
+print arithmetic(2, 2, 4)
