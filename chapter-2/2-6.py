d84a2a625e5d646f6a0b1965edef75d6563e3b64
JP Doherty <jpdoherty90@gmail.com>
2017-10-07 13:52:29 -0500

    added 2-6

---
+++ chapter-2/2-6.py
@@ -0,0 +1,51 @@
+class CreditCard:
+    
+    def __init__(self, customer, bank, acnt, limit):
+
+        self._customer = customer
+        self._bank = bank
+        self._account = acnt
+        self._limit = limit
+        self._balance = 0
+
+    def get_customer(self):
+
+        return self._customer
+
+    def get_bank(self):
+
+        return self._bank
+
+    def get_account(self):
+
+        return self._account
+
+    def get_limit(self):
+
+        return self._limit
+
+    def get_balance(self):
+
+        return self._balance
+
+    def charge(self, price):
+        
+        if isinstance(price, (int, float)):
+            if price + self._balance > self._limit:
+                return False
+            else:
+                self._balance += price
+                return True
+        raise TypeError("Price must be a number")
+    
+    def make_payment(self, amount):
+        
+        if isinstance(amount, (int, float)):
+            if amount >= 0: 
+                self._balance -= amount
+            else:
+                raise ValueError("Payment amount cannot be negative")
+        else:
+            raise TypeError("Payment amount must be numeric")
+
+    
